a6aa194e8409d06c4fc111df004825464a4388f8
qiujiangkun <qiujiangkun@foxmail.com>
2021-12-23 23:15:05 +0800

    setup.py & gitignore & documents

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # TypedModel
 
-
 TypedModel aims to provide strict type checking for dataclass and pydantic model.
 
 dataclass is useful, but provides no type checking
@@ -11,3 +10,23 @@ https://github.com/samuelcolvin/pydantic/issues/3189
 
 https://github.com/samuelcolvin/pydantic/issues/3569
 
+## Usage
+
+```shell
+pip install typedmodel
+```
+
+```python3
+from typedmodel import BaseModel
+
+
+class Foo(BaseModel):
+    a: str
+    b: str
+    d = 'default'
+    e: str = 'default'
+
+
+foo = Foo(a="a", b="b")
+
+```
---
+++ setup.py
@@ -0,0 +1,30 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="typedmodel",
+    version="0.1.0",
+    description="provide strict type checking for dataclass and pydantic model",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/qiujiangkun/typedmodel",
+    author="Jiangkun QIU",
+    author_email="qjk2001@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["typedmodel"],
+    include_package_data=True,
+    install_requires=["beartype"],
+    entry_points={},
+)
