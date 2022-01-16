910d122d2ad601f4989fecdc081f96cfb0db7dee
qiujiangkun <qiujiangkun@foxmail.com>
2022-01-16 21:37:14 +0800

    0.2.6

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="typedmodel",
-    version="0.2.5",
+    version="0.2.6",
     description="provide strict type checking for dataclass and pydantic model",
     long_description=README,
     long_description_content_type="text/markdown",
